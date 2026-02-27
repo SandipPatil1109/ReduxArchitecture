4d1745ad00b72430885173b340234a98754c4061
Sandeep Patil <snsan1109@gmail.com>
2026-02-27 16:16:57 +0530

    changes R1

--- app/src/main/java/com/example/sampleapp/redux/action/UpdateTimeAction.kt
+++ app/src/main/java/com/example/sampleapp/redux/action/UpdateTimeAction.kt
@@ -3,9 +3,6 @@ package com.example.sampleapp.redux.action
 sealed class UpdateTimeAction {
 //this is the one example
 
-    //that cal
-
-
 
 
     data class UpdateMessage(val message: String) : UpdateTimeAction()
