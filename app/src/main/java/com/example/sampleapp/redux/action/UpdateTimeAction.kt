d319500b2e21bffea9ebd25d1d8bed280be48977
Sandeep Patil <snsan1109@gmail.com>
2026-02-27 15:56:29 +0530

    upadateAction

--- app/src/main/java/com/example/sampleapp/redux/action/UpdateTimeAction.kt
+++ app/src/main/java/com/example/sampleapp/redux/action/UpdateTimeAction.kt
@@ -1,6 +1,12 @@
 package com.example.sampleapp.redux.action
 
 sealed class UpdateTimeAction {
+//this is the one example
+
+    //that can i show
+
+
+
 
     data class UpdateMessage(val message: String) : UpdateTimeAction()
     object ClearMessage : UpdateTimeAction()
