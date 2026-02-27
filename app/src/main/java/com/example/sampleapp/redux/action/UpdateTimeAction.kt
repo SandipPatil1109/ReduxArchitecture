58a61bae4b4f5bab8960a6fdf6819cb394c9436f
Sandeep Patil <snsan1109@gmail.com>
2026-02-27 15:29:48 +0530

    Update time action file changed

--- app/src/main/java/com/example/sampleapp/redux/action/UpdateTimeAction.kt
+++ app/src/main/java/com/example/sampleapp/redux/action/UpdateTimeAction.kt
@@ -2,8 +2,6 @@ package com.example.sampleapp.redux.action
 
 sealed class UpdateTimeAction {
 
-    //changes add
-
     data class UpdateMessage(val message: String) : UpdateTimeAction()
     object ClearMessage : UpdateTimeAction()
 
